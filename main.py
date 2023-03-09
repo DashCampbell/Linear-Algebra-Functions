9f68391713ef8d6369ef76d8ab5aa9dc4a7e7431
DashCampbell <dash9campbell@gmail.com>
2023-03-09 13:48:42 -0700

    added cross product

--- main.py
+++ main.py
@@ -15,10 +15,29 @@ def mat2x2Det(M):
 def mat3x3Det(M):
     return (M[0][0]*mat2x2Det([[M[1][1], M[1][2]],
                                [M[2][1], M[2][2]]])
-            - M[0][1]*mat2x2Det([[M[1][0], M[2][0]],
-                                 [M[1][2], M[2][2]]])
-            + M[0][2]*mat2x2Det([[M[1][0], M[2][0]],
-                                 [M[1][1], M[2][1]]]))
+            - M[0][1]*mat2x2Det([[M[1][0], M[1][2]],
+                                 [M[2][0], M[2][2]]])
+            + M[0][2]*mat2x2Det([[M[1][0], M[1][1]],
+                                 [M[2][0], M[2][1]]]))
+
+# u,v are R3 vectors
+
+
+def cross(u, v):
+    return [mat2x2Det([[u[1], u[2]],
+                       [v[1], v[2]]]),
+            -mat2x2Det([[u[0], u[2]],
+                       [v[0], v[2]]]),
+            mat2x2Det([[u[0], u[1]],
+                       [v[0], v[1]]])]
+
+
+def lenCross(u, v):
+    return lenVec(cross(u, v))
+
+
+def scalarTripleProduct(s, u, v):
+    return dot(s, cross(u, v))
 
 
 def mulVec(s, vec):
@@ -26,6 +45,28 @@ def mulVec(s, vec):
     for i in range(len(vec)):
         newV.append(vec[i]*s)
     return newV
+
+
+def subVec(u, v):
+    newV = []
+    for i in range(len(u)):
+        newV.append(u[i]-v[i])
+    return newV
+
+
+def addVec(u, v):
+    newV = []
+    for i in range(len(u)):
+        newV.append(u[i]+v[i])
+    return newV
+
+
+def lenVec(v):
+    return math.sqrt(dot(v, v))
+
+
+def unitVec(v):
+    return mulVec(1/lenVec(v), v)
 
 
 def matGetColumn(M, n):
@@ -83,3 +124,12 @@ def mat3x3Inverse(M):
 
 def proj(u1, v1):
     return mulVec((dot(u1, v1)/dot(u1, u1)), u1)
+
+
+u = [-2, 1, -1]
+v = [-1, -1, 1]
+w = [1, 1, 8]
+
+n = cross(u, v)
+unit_n = unitVec(cross(u, v))
+print(mulVec(dot(unit_n, w), unit_n))
