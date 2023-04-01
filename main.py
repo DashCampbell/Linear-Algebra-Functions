e47bc03ee1c9cbf5647d1b3851a4648ca7644d20
DashCampbell <dash9campbell@gmail.com>
2023-04-01 15:55:09 -0600

    changed formatting

--- main.py
+++ main.py
@@ -5,6 +5,55 @@ import math
 #####################################
 
 
+def matGetColumn(M, c):
+    col = []
+    for i in range(len(M)):
+        col.append(M[i][c])
+    return col
+
+
+def matMulMatrices(M1, M2):
+    newMat = []
+    for row in range(len(M1)):
+        newMat.append([])
+        for col in range(len(M2[0])):
+            newMat[row].append(dot(M1[row], matGetColumn(M2, col)))
+    return newMat
+
+
+# # Multiply Multiple Matrices
+# matrics - array of matrices
+def matMulMatrices2(matrices, M):
+    if len(matrices) <= 1:
+        return matMulMatrices(matrices[0], M)
+    return matMulMatrices2(matrices[0:-1], matMulMatrices(matrices[-1], M))
+
+
+def matMulScalar(M, s):
+    newMat = []
+    for row in range(len(M)):
+        newMat.append([])
+        for col in range(len(M[0])):
+            newMat[row].append(M[row][col] * s)
+    return newMat
+
+
+def matAddMatrices(M1, M2):
+    newMat = []
+    for row in range(len(M1)):
+        newMat.append([])
+        for col in range(len(M1[0])):
+            newMat[row].append(M1[row][col] + M2[row][col])
+    return newMat
+
+
+def matTranspose(M):
+    newMat = []
+    for col in range(len(M[0])):
+        newMat.append(matGetColumn(M, col))
+    return newMat
+
+
 def mat2x2Det(M):
     return M[0][0] * M[1][1] - M[0][1] * M[1][0]
 
@@ -189,55 +238,6 @@ def lenVec(v):
 
 def unitVec(v):
     return mulVec(1 / lenVec(v), v)
-
-
-def matGetColumn(M, c):
-    col = []
-    for i in range(len(M)):
-        col.append(M[i][c])
-    return col
-
-
-def matMulMatrices(M1, M2):
-    newMat = []
-    for row in range(len(M1)):
-        newMat.append([])
-        for col in range(len(M2[0])):
-            newMat[row].append(dot(M1[row], matGetColumn(M2, col)))
-    return newMat
-
-
-# # Multiply Multiple Matrices
-# matrics - array of matrices
-def matMulMatrices2(matrices, M):
-    if len(matrices) <= 1:
-        return matMulMatrices(matrices[0], M)
-    return matMulMatrices2(matrices[0:-1], matMulMatrices(matrices[-1], M))
-
-
-def matMulScalar(M, s):
-    newMat = []
-    for row in range(len(M)):
-        newMat.append([])
-        for col in range(len(M[0])):
-            newMat[row].append(M[row][col] * s)
-    return newMat
-
-
-def matAddMatrices(M1, M2):
-    newMat = []
-    for row in range(len(M1)):
-        newMat.append([])
-        for col in range(len(M1[0])):
-            newMat[row].append(M1[row][col] + M2[row][col])
-    return newMat
-
-
-def matTranspose(M):
-    newMat = []
-    for col in range(len(M[0])):
-        newMat.append(matGetColumn(M, col))
-    return newMat
 
 
 def proj(v1, u1):
